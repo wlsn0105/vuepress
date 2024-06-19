08bd96f64974c739cdd50715b4f026ba06c3fb3a
wlsn0105 <jack13579@naver.com>
2024-06-19 16:14:16 +0900

    Feat: 메인 브랜치에 푸시

---
+++ docs/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build commands
+yarn build
+
+# navigate into the build output directory
+cd .vuepress/dist
+
+git init
+git add -A
+git commit -m "deploy with vuepress script"
+git push -f https://github.com/wlsn0105/vuepress.git main:gh-pages
+
+cd -
